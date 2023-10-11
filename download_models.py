242f0f462609d62baba8871338a2cd3303d999af
Ubuntu <azureuser@apex-iXBRL-tool.0jjzvulf3esuba2x5xhkuf530g.rx.internal.cloudapp.net>
2023-10-11 09:52:54 +0000

    .sh file adde

---
+++ download_models.py
@@ -0,0 +1,5 @@
+import gdown
+
+id = "19YH7asEO2P_G_LAdpiKGGT9Sf-uTGMSy"
+output = "Models"
+gdown.download_folder(id=id, output=output, quiet=False)
---
+++ download_models.sh
@@ -0,0 +1,2 @@
+python3 -m pip install gdown
+python3 download_models.py
