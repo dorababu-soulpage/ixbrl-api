98ff7a456bb8c086bab2ff2a9295d3aac8ea7675
dorababu-soulpage <dorababu.alapakam@soulpageit.com>
2023-10-12 17:15:53 +0530

    auto-tagging changes

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from utils import (
     get_filename
 )
 from threading import Thread
-from auto_tagging.tagging import auto_tagging
+# from auto_tagging.tagging import auto_tagging
 from flask import Flask, request, redirect, url_for
 
 
@@ -211,51 +211,12 @@ def read_html_tagging_file():
     else:
         return {"error":"html file Not found"}, 400
     
-
-def auto_tagging_thread(file_id:int):
-    record = get_db_record(file_id = file_id)
-    html = record.get("extra").get("url", "")
-    output_dir = f"{storage_dir}/html/{Path(html).stem}".replace("_","-")
-    # create viewer folder
-    Path(f"{output_dir}").mkdir(parents=True, exist_ok=True)
-    filename = f"{output_dir}/{Path(html).stem}.html"
-        # Send an HTTP GET request to the URL
-    response = requests.get(html)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Get the content from the response
-        html_content = response.text
-
-        # Write the content to a local file
-        with open(filename, "w") as file:
-            file.write(html_content)
-    
-        output_html = auto_tagging(filename)
-        try:
-            with open(output_html, "rb") as file:
-                body = io.BytesIO(file.read())
-                # Parse the URL to extract the path
-                parsed_url = urlsplit(output_html)
-                # Get the filename from the path using pathlib
-                path = Path(parsed_url.path)
-                filename = path.name
-                url = s3_uploader(name=filename, body=body)
-                update_db_record(file_id, {"auto_tagging_url":url})
-        except Exception as e:
-            return {"error": "auto_tagging_html file is not generated"}, 400
-    
-
 @app.route("/api/auto-tagging", methods=["POST"])
 def auto_tagging_view():
-    
     file_id = request.json.get("file_id", None)
-    # run process in background
-    thread = Thread(target=auto_tagging_thread, args=(file_id,))
-    thread.start()
-    return {"message":"We will notify you once auto tagging is done."} , 200
-
-
+    url = config("AUTO_TAGGING_URL")
+    response = requests.post(url, json={'file_id': file_id})
+    return response.json() , response.status_code
 
 if __name__ == "__main__":
     port = config("PORT")
